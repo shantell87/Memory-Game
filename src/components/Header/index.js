417f2c6aa7056f2aef74d5c3fdb8905df27f3de3
Shantell Davis <shantell87@gmail.com>
2019-08-15 00:07:35 -0700

    updated images and bcg photo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./style.css";
+import "./../Header/style.css";
 
 function Header() {
   return (
     <header className="header">
-      <h1>Clicky Game!</h1>
+      <h1>Memory Game!</h1>
       <h2>Click on an image to earn points, but don't click on any more than once!</h2>
     </header>
   );
